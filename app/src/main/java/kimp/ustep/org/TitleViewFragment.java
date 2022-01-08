32df397f51306cc0418abf4c8b24bc8d31112274
KonstantIMP <mihedovkos@gmail.com>
2022-01-08 22:39:50 +0300

    Create TitleViewFragment sceleton

---
+++ app/src/main/java/kimp/ustep/org/TitleViewFragment.java
@@ -0,0 +1,13 @@
+package kimp.ustep.org;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Fragment for title and logo display
+ */
+public class TitleViewFragment extends Fragment {
+
+    public TitleViewFragment() {
+        super(R.layout.title_view_fragment);
+    }
+}
