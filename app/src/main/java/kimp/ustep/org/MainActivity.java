aa0f46f5c06e3420bada7076acbda1f01d6c0a9c
KonstantIMP <mihedovkos@gmail.com>
2022-01-08 22:22:01 +0300

    Base MainActivity changes

--- app/src/main/java/kimp/ustep/org/MainActivity.java
+++ app/src/main/java/kimp/ustep/org/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Start page for the application
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
     }
 }
