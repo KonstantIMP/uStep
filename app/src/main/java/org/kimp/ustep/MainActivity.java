3cb88033428d1c4f5a414a72844045bdb15d4ebb
KonstantIMP <mihedovkos@gmail.com>
2022-01-09 17:34:35 +0300

    Add OptionsActivity calls

--- app/src/main/java/org/kimp/ustep/MainActivity.java
+++ app/src/main/java/org/kimp/ustep/MainActivity.java
@@ -1,13 +1,16 @@
 package org.kimp.ustep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentResultListener;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
  * Start page for the application
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentResultListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+        getSupportFragmentManager().setFragmentResultListener("requestKey", this, this);
+    }
+
+    /**
+     * Open next activity if ActionFragment send command
+     */
+    @Override
+    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+        if (result.getString("action").equals("options")) {
+            startActivity(new Intent(this, OptionsActivity.class));
+        }
+        else if (result.getString("action").equals("universities")) {
+            // TODO
+        }
     }
 }
--- app/src/main/java/org/kimp/ustep/fragment/ActionViewFragment.java
+++ app/src/main/java/org/kimp/ustep/fragment/ActionViewFragment.java
@@ -1,6 +1,16 @@
 package org.kimp.ustep.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
 
 import org.kimp.ustep.R;
 
@@ -11,5 +21,30 @@ public class ActionViewFragment extends Fragment {
 
     public ActionViewFragment() {
         super(R.layout.action_view_fragment);
+    }
+
+    @Nullable @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.action_view_fragment, container, false);
+
+        view.findViewById(R.id.optionsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle action = new Bundle();
+                action.putString("action", "options");
+                getParentFragmentManager().setFragmentResult("requestKey", action);
+            }
+        });
+
+        view.findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle action = new Bundle();
+                action.putString("action", "universities");
+                getParentFragmentManager().setFragmentResult("requestKey", action);
+            }
+        });
+
+        return view;
     }
 }
