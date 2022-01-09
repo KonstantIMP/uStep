0848e47e16984ed4c77c2f38cfe19fe035012ebb
KonstantIMP <mihedovkos@gmail.com>
2022-01-09 11:57:05 +0300

    Fully finished title and logo fragment

--- app/src/main/java/org/kimp/ustep/MainActivity.java
+++ app/src/main/java/org/kimp/ustep/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
     }
 }
