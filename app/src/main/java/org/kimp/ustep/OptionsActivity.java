3d10a6ae78395852d87d485dad932cf05cbca621
KonstantIMP <mihedovkos@gmail.com>
2022-01-12 00:56:02 +0300

    Add base options description

--- app/src/main/java/org/kimp/ustep/OptionsActivity.java
+++ app/src/main/java/org/kimp/ustep/OptionsActivity.java
@@ -1,14 +1,35 @@
 package org.kimp.ustep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.os.Bundle;
 
+/**
+ * Activity for the app's managing
+ */
 public class OptionsActivity extends AppCompatActivity {
+
+    public static class OptionsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        try {
+            getSupportActionBar().setTitle(R.string.options_msg);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.options_fragment_holder, new OptionsFragment())
+                    .commit();
+        } catch (NullPointerException e) {}
     }
 }
